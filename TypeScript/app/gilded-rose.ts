411eb92d5cda312212150e818505c04445843d99
MarRobSoftwire <mark.Roberts@softwire.com>
2022-07-06 15:41:06 +0100

    neatened up disorganised if statements

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -30,15 +30,10 @@ export class GildedRose {
         if (this.items[i].quality < 50) {
           incrementQuality(this.items[i],1);
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                incrementQuality(this.items[i],1);
-              }
-            }
             if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
+                incrementQuality(this.items[i],2);
+            }else if (this.items[i].sellIn < 11) {
                 incrementQuality(this.items[i],1);
-              }
             }
           }
         }
@@ -47,20 +42,12 @@ export class GildedRose {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                incrementQuality(this.items[i],-1);
-              }
-            }
-          } else {
-            this.items[i].quality = 0;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            incrementQuality(this.items[i],1);
-          }
+        if (this.items[i].name == 'Aged Brie'&& this.items[i].quality < 50){
+          incrementQuality(this.items[i],1);
+        } else if(this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'){
+          this.items[i].quality = 0;
+        } else if(this.items[i].quality > 0 && this.items[i].name != 'Sulfuras, Hand of Ragnaros'){          
+          incrementQuality(this.items[i],-1);
         }
       }
     }
